4866ac3a37e527af004146f5547122b6b2335fb9
ra100 <ra100x@gmail.com>
2016-01-29 19:06:11 +0100

    wifi changes

--- src/wifi.lua
+++ src/wifi.lua
@@ -2,8 +2,9 @@ ip = wifi.sta.getip()
 print(ip)
 if ip == nil then
   wifi.setmode(wifi.STATION)
+  wifi.setphymode(wifi.PHYMODE_N)
   wifi.sta.config(SSID, PASSWORD, 1)
-  wifi.sleeptype(wifi.NONE_SLEEP)
+  wifi.sleeptype(wifi.LIGHT_SLEEP)
   wifi.sta.autoconnect(1)
   wifi.sta.connect()
 end
